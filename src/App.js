aa0bc2ef5730e51ebda6da44132b62e0ae854f90
Santos <realsantosm@gmail.com>
2021-11-20 11:37:10 +0530

    chores : installed packages

---
+++ src/App.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function MSKTodos(){
+    return <div>Hello World</div>
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from "react";
 import ReactDom from "react-dom";
 
+import {Provider} from "react-redux";
+import {store} from "./redux/store";
+import ConnectedApp from "./app";
+
+const ReduxApp = () => {
+  return <Provider store={store}>
+    <ConnectedApp />
+  </Provider>;
+};
+
 const App = ()=>{
-  return <div>Hello-World</div>
+  return <div>Hello World</div>
 }
 
-ReactDom.render(<App />, document.getElementById("root"));
+ReactDom.render(<ReduxApp />, document.getElementById("root"));
